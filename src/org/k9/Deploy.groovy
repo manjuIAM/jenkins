9c676c39e4d106173c5a608c8a855ee5bda1c42a
manjuIAM <34343177+manjuIAM@users.noreply.github.com>
2018-02-02 10:28:15 +0530

    Create Deploy.groovy

---
+++ src/org/k9/Deploy.groovy
@@ -0,0 +1,20 @@
+package org.k9.deploy
+
+class Deploy{
+    def config
+    def script
+    
+    Deploy(script, config){
+    this.cofig =config
+    this.script=script
+    }
+    
+    void deployimage(){
+    this.script.stage('Deploy Image in Openshift'){
+      openshift.withCluster(this.config.clustername){
+        openshift.newBuild("--name=this.config.imagename", "--image-stream=this.config.dockerurl", "--binary")
+        openshift.newApp("this.config.imagename:latest" "--name=this.config.appname").narrow('svc').expose()
+        }
+        
+     }
+    }
